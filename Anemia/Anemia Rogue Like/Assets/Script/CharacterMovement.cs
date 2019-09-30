9f00758ea991c789d6c45d44956c46818851f663
Arthur <arthurgro@outlook.fr>
2019-09-30 13:22:32 +0200

    Base du projet unity
    Contient une scène pour Arthur et le script de déplacement

--- Anemia/Anemia Rogue Like/Assets/Script/CharacterMovement.cs
+++ Anemia/Anemia Rogue Like/Assets/Script/CharacterMovement.cs
@@ -25,12 +25,4 @@ public class CharacterMovement : MonoBehaviour
         transform.position = transform.position + movement * acceleration * Time.deltaTime;
     }
 
-    //dash qui ne prend pas en compte les colisions, à modifier (button 1 = O)
-    public void Dash()
-    {
-        if (Input.GetButton("Dash"))
-        {
-            
-        }
-    }
 }
