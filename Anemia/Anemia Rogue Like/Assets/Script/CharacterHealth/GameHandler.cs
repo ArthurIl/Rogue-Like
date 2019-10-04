ce7195f6098ac14490631f63d0dd83aec4bc8c5d
Bastien <B.prigent@rubika-edu.com>
2019-10-04 18:45:26 +0200

    Proto 1 Mouvement + barre de vie
    Début création mécha principale, vie qui se perd et remonte lors de l'appuie sur une touche

---
+++ Anemia/Anemia Rogue Like/Assets/Script/CharacterHealth/GameHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    [SerializeField]
+    private HealthBar healthbar;
+    [SerializeField]
+    private float health = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+     void Update()
+     {
+
+     }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        healthdown();
+        healthUp();
+    }
+ 
+   public void healthUp()
+    {
+        if (health < 1 && Input.GetKey(KeyCode.Y))
+        {
+            health += .01f;
+            healthbar.setSize(health);
+        }
+       
+    }
+    public void healthdown()
+    {
+        if (health > 0)
+        {
+            health -= .001f;
+            healthbar.setSize(health);
+        }
+    }
+}
