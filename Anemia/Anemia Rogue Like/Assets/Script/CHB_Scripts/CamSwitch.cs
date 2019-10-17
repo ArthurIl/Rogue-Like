3a5c04244c3e41030fb07ae54fc2102c9a5b23ac
Charles Bentein <c.bentein@rubika-edu.com>
2019-10-17 16:54:07 +0200

    Y a un switch de caméra, mais pas de switch back

---
+++ Anemia/Anemia Rogue Like/Assets/Script/CHB_Scripts/CamSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamSwitch : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject playerCam;
+    public GameObject roomCam;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            roomCam.SetActive(true);
+            playerCam.SetActive(false);
+        }
+    }
+}
