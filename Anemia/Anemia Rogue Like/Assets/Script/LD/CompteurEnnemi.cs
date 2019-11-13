42ddde7d1995bf230753a8f07215d7a561ae9740
Steve Guitton <s.guitton@rubika-edu.com>
2019-11-13 16:46:44 +0100

    Modif stats + Compteur d'ennemi
    Augmentation de la vitesse de l'empalé + compteur d'ennmi qui désactive le collider de la porte de sortie une fois à 0

---
+++ Anemia/Anemia Rogue Like/Assets/Script/LD/CompteurEnnemi.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompteurEnnemi : MonoBehaviour
+{
+    private int finalCountdown;
+    private Collider2D exitDoor;
+    private GameObject door;
+    public GameObject[] ennemies;
+    void Start()
+    {
+        door = GameObject.FindGameObjectWithTag("ExitDoor");
+        exitDoor = door.GetComponent<Collider2D>();
+       
+    }
+
+    private void Update()
+    {
+        ennemies = GameObject.FindGameObjectsWithTag("Ennemi");
+        if (ennemies.Length == 0)
+        {
+            exitDoor.enabled = !exitDoor.enabled;
+        }
+    }
+  
+}
