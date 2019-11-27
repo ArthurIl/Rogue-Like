e36b93b8fec7ac78d14e7257d0fda52e5930f6d7
Charles Bentein <c.bentein@rubika-edu.com>
2019-11-27 11:06:42 +0100

    Eh ben mes consommables fonctionnent on dirait
    Plus qu'à mettre les bon sprite
    Run avec la console en collapse, y a des bons retours

--- Anemia/Anemia Rogue Like/Assets/Script/CHB_Scripts/Items/DisposableBehavior.cs
+++ Anemia/Anemia Rogue Like/Assets/Script/CHB_Scripts/Items/DisposableBehavior.cs
@@ -24,15 +24,21 @@ public class DisposableBehavior : MonoBehaviour
             if (myDisposable.healthRefill > 0)
             {
                 playerHandler.TakeDamage(0 - myDisposable.healthRefill);
-                Debug.Log("Health refilled of:" + myDisposable.healthRefill);
+                Debug.Log("Health refilled of: " + myDisposable.healthRefill);
             }
             else if (myDisposable.dammageBoost > 0)
             {
+                Debug.Log("Attack damage : " + playerAttackDam + ",");
                 playerAttackDam = myDisposable.HitMore(playerAttackDam);
+                Debug.Log("Has become : " + playerAttackDam);
+
             }
             else if (myDisposable.soulsAdd > 0)
             {
+                Debug.Log("Souls count : " + playerSouls + ",");
                 playerSouls = myDisposable.GiveSouls(playerSouls);
+                Debug.Log("Has become : " + playerSouls);
+
             }
 
             Destroy(gameObject);
