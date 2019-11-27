5cd9062337c53cbfddafc11ef3508057a8443abf
Steve Guitton <s.guitton@rubika-edu.com>
2019-11-27 11:52:27 +0100

    Optimise Procédural
    Procédural ver 2.5

---
+++ Anemia/Anemia Rogue Like/Assets/Script/Items/Bottes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bottes : MonoBehaviour
+{
+    private float speed;
+    public float multiplier;
+    public CharacterMovement script;
+
+    private void Start()
+    {
+        script = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterMovement>();
+        script.haveBoot = true;
+        script.acceleration = script.acceleration * multiplier;
+    }
+}
--- Anemia/Anemia Rogue Like/Assets/Script/LD/Room/Teleporter.cs
+++ Anemia/Anemia Rogue Like/Assets/Script/LD/Room/Teleporter.cs
@@ -8,15 +8,18 @@ public class Teleporter : MonoBehaviour
     public GameObject entrance;
     public GameObject room;
     public GameObject spawnerEntrance;
+    private RoomCreation spawnedGet;
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         entrance = GameObject.FindGameObjectWithTag("Teleporter");
+        spawnedGet = GameObject.FindGameObjectWithTag("Teleporter").GetComponent<RoomCreation>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        spawnedGet.spawned = false;
         spawnerEntrance = GameObject.FindGameObjectWithTag("SpawnerEntrance");
         entrance.transform.position = spawnerEntrance.transform.position;
         player.transform.position = entrance.transform.position;
