290582b11a93877ba06318cd58e69f666f5210c4
Arthur <arthurgro@outlook.fr>
2019-12-08 12:07:04 +0100

    HUB & Item
    Branche du HUB pour le merge

--- Anemia/Anemia Rogue Like/Assets/Script/HUB/Items/ARGShop4.cs
+++ Anemia/Anemia Rogue Like/Assets/Script/HUB/Items/ARGShop4.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ARGShop : MonoBehaviour
+public class ARGShop4 : MonoBehaviour
 {
-    public static ARGShop Instances { get; private set; }
+    public static ARGShop4 Instances { get; private set; }
     public int bloods;
     public int price;
     private int newBloods;
     public GameObject thisObject;
+    public GameObject itemUnlocked;
     private List<GameObject> thisList;
-    private bool canBuy;
+    private bool canBuy4;
     // Start is called before the first frame update
 
     private void Awake()
@@ -28,15 +29,14 @@ public class ARGShop : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (canBuy == true && Input.GetButton("Drain"))
+        if (canBuy4 == true && Input.GetButton("Drain"))
         {
-            
-            Debug.Log("Buying");
+
             newBloods = bloods - price;
             GameManager.Instance.bloodCount = newBloods;
             Debug.Log(newBloods);
             thisObject.SetActive(false);
-            GameManager.Instance.ItemList(this.gameObject);
+            GameManager.Instance.ItemList(itemUnlocked);
             DontDestroyOnLoad(this.gameObject);
         }
     }
@@ -44,30 +44,18 @@ public class ARGShop : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         bloods = GameManager.Instance.bloodCount;
-
-        //if (bloods >= price)
-        //{
-        //    Debug.Log("I can buy");
-        //    canBuy = true;
-        //}
-        //else
-        //{
-        //    Debug.Log("I can't buy");
-        //    canBuy = false;
-        //}
-
-    }
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
         if (bloods >= price)
         {
-            canBuy = true;
+            canBuy4 = true;
         }
         else
         {
-            canBuy = false;
+            canBuy4 = false;
         }
+    }
 
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        canBuy4 = false;
     }
 }
