9176323d134cd48645338891cef2034bba68fe19
Charles Bentein <c.bentein@rubika-edu.com>
2019-12-13 11:45:43 +0100

    Main Menu il est là

---
+++ Anemia/Anemia Rogue Like/Assets/Script/CHB_Scripts/Menus/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainScreen;
+    public GameObject credScreen;
+
+    private void Start()
+    {
+        mainScreen.SetActive(true);
+        credScreen.SetActive(false);
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("ARG HUB");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void ToCredits()
+    {
+        mainScreen.SetActive(false);
+        credScreen.SetActive(true);
+    }
+
+    public void Back()
+    {
+        mainScreen.SetActive(true);
+        credScreen.SetActive(false);
+    }
+}
