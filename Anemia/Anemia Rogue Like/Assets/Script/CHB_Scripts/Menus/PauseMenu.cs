33c89c06309092a728c5877dc76eca07c350b9ac
Charles Bentein <c.bentein@rubika-edu.com>
2019-12-13 12:00:34 +0100

    Working on pause script

--- Anemia/Anemia Rogue Like/Assets/Script/CHB_Scripts/Menus/PauseMenu.cs
+++ Anemia/Anemia Rogue Like/Assets/Script/CHB_Scripts/Menus/PauseMenu.cs
@@ -1,18 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject pauseScreen;
+    private bool pauseToggled = false;
     void Start()
+    {
+        pauseScreen.SetActive(false);
+    }
+
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TogglePause()
     {
-        
+        if (!pauseToggled)
+        {
+            pauseToggled = true;
+            Time.timeScale = 0f;
+            pauseScreen.SetActive(true);
+        }
+        else
+        {
+            Resume();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseScreen.SetActive(false);
+        pauseToggled = false;
+        Time.timeScale = 1f;
+    }
+
+    public void ToHub()
+    {
+        SceneManager.LoadScene("ARG HUB");
+    }
+
+    public void ToMainMenu()
+    {
+        SceneManager.LoadScene("CHB_MainMenu");
     }
 }
