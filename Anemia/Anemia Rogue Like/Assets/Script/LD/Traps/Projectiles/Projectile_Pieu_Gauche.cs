3b909c6b93c1a03dc28a4d65d24994195bd8a289
Arthur <arthurgro@outlook.fr>
2019-12-15 18:29:13 +0100

    Intégration des sprites
    Intégration des sprites d'items, de piège et d'UI

---
+++ Anemia/Anemia Rogue Like/Assets/Script/LD/Traps/Projectiles/Projectile_Pieu_Gauche.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile_Pieu_Gauche : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1f;
+    public float damage;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            col.gameObject.GetComponent<GameHandler>().TakeDamage(damage);
+            Destroy(gameObject);
+        }
+
+        if (col.gameObject.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
