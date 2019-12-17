0fb967cf42458582db9cb8abd6e8974c03e09e98
Steve Guitton <s.guitton@rubika-edu.com>
2019-12-17 13:04:13 +0100

    Fix du LD et intégration du procédural

--- Anemia/Anemia Rogue Like/Assets/Script/LD/Room/RoomCreation.cs
+++ Anemia/Anemia Rogue Like/Assets/Script/LD/Room/RoomCreation.cs
@@ -9,6 +9,7 @@ public class RoomCreation : MonoBehaviour
     public int roomNumber = 0;
     public GameObject spawner;
     public bool spawned;
+    public GameObject spawnerEntrance;
 
     private void Start()
     { 
@@ -20,38 +21,41 @@ public class RoomCreation : MonoBehaviour
     {
         if (spawned == false)
         {
-            if (roomNumber < 4)
+            if (roomNumber < 2)
             {
 
-                rand = Random.Range(0, template.easyRooms.Length);
+                rand = Random.Range(0, template.easyRooms.Count);
                 Instantiate(template.easyRooms[rand], spawner.transform.position, template.easyRooms[rand].transform.rotation);
-
+                template.easyRooms.RemoveAt(rand);
             }
-            else if (roomNumber == 4)
+            else if (roomNumber == 3)
             {
 
-                rand = Random.Range(0, template.shop.Length);
+                rand = Random.Range(0, template.shop.Count);
                 Instantiate(template.shop[rand], spawner.transform.position, template.shop[rand].transform.rotation);
             }
-            else if (roomNumber > 4 & roomNumber < 7)
+            else if (roomNumber > 3 & roomNumber < 6)
             {
-                rand = Random.Range(0, template.mediumRooms.Length);
+                rand = Random.Range(0, template.mediumRooms.Count);
                 Instantiate(template.mediumRooms[rand], spawner.transform.position, template.mediumRooms[rand].transform.rotation);
+                template.mediumRooms.RemoveAt(rand);
             }
-            else if (roomNumber == 7)
+            else if (roomNumber == 6)
             {
-                rand = Random.Range(0, template.shop.Length);
+                rand = Random.Range(0, template.shop.Count);
                 Instantiate(template.shop[rand], spawner.transform.position, template.shop[rand].transform.rotation);
             }
-            else if (roomNumber > 7 & roomNumber < 10)
+            else if (roomNumber > 6 & roomNumber < 8)
             {
-                rand = Random.Range(0, template.hardRooms.Length);
+                rand = Random.Range(0, template.hardRooms.Count);
                 Instantiate(template.hardRooms[rand], spawner.transform.position, template.hardRooms[rand].transform.rotation);
+                template.hardRooms.RemoveAt(rand);
             }
             else
             {
-                rand = Random.Range(0, template.bossRooms.Length);
+                rand = Random.Range(0, template.bossRooms.Count);
                 Instantiate(template.bossRooms[rand], spawner.transform.position, template.bossRooms[rand].transform.rotation);
+                template.bossRooms.RemoveAt(rand);
             }
             roomNumber += 1;
             Destroy(spawner.gameObject);
@@ -59,13 +63,17 @@ public class RoomCreation : MonoBehaviour
         }                
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D col)
     {
-        if (collision.gameObject.tag == "Player")
+        if (col.gameObject.tag == "Player")
         {
+            
             spawner = GameObject.FindGameObjectWithTag("Spawner");
             Spawn();
-
+            spawnerEntrance = GameObject.FindGameObjectWithTag("SpawnerEntrance");
+            transform.position = spawnerEntrance.transform.position;
+            Destroy(spawnerEntrance.gameObject);
+            col.transform.position = transform.position;
         }
             
     }
